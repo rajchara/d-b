751834b9b1121e6e9d4082db7e4464664a3cd51b
rajchara <111902730+rajchara@users.noreply.github.com>
2025-10-28 16:29:59 -0400

    Update outputs.tf

--- groups/outputs.tf
+++ groups/outputs.tf
@@ -1,21 +1,29 @@
 output "databricks_group" {
-  description = "Map of created Databricks account-level groups"
+  description = "Created Databricks account-level group"
   value       = databricks_group.this
 }
 
-output "databricks_group_member" {
-  description = "Map of user and service principal assignments to groups"
-  value       = databricks_group_member.this
+output "databricks_group_member_users" {
+  description = "User assignments to group"
+  value       = databricks_group_member.users
+}
+
+output "databricks_group_member_service_principals" {
+  description = "Service principal assignments to group"
+  value       = databricks_group_member.service_principals
+}
+
+output "databricks_group_member_groups" {
+  description = "Group assignments to group"
+  value       = databricks_group_member.groups
 }
 
 output "databricks_mws_permission_assignment" {
-  description = "Map of group assignments to workspaces"
+  description = "Group assignments to workspaces"
   value       = databricks_mws_permission_assignment.this
 }
 
-output "group_ids" {
-  description = "Map of group names to their IDs"
-  value = {
-    for k, v in databricks_group.this : k => v.id
-  }
+output "group_id" {
+  description = "Group ID"
+  value       = databricks_group.this.id
 }
